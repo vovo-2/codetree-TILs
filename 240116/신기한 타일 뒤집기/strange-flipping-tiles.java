65fa165043f1a53225f526d1ac5bc05b34bb8896
vovo-2 <153492321+vovo-2@users.noreply.github.com>
2024-01-16 23:22:47 +0900

    Title: 신기한 타일 뒤집기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240116/신기한 타일 뒤집기/strange-flipping-tiles.java
+++ 240116/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -10,29 +10,22 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int now = 100000;
 
-        int x1 =0;
-        int x2 =0;
-
         for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
             
             int x = Integer.parseInt(st.nextToken());
             String order = st.nextToken();
 
-            if(order.equals("R")){
-                x1 = now;
-                x2 = now+x;
-
-                now+=x;
-
-                for(int j=x1;j<x2;j++) line[j]=1;
-            }else{
-                x1 = now-x;
-                x2 = now;
-
-                now-=x;
-                for(int j=x1;j<x2;j++) line[j]=-1;
+            for(int t=0;t<x;t++){
+                if(order.equals("R")){
+                    line[now++]=1;
+                }else{
+                    line[now--]=-1;
+                }
             }
+
+            if(order.equals("R")) now--;
+            else now++;
 
         }
 
