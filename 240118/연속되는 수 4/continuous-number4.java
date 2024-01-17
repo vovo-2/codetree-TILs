ba4998839ad93b0cae4a14d234790d0fa32cdab9
vovo-2 <153492321+vovo-2@users.noreply.github.com>
2024-01-18 08:37:27 +0900

    Title: 연속되는 수 4, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240118/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+//System.out.println();
+
+public class Main {
+    static BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+
+        int num[] = new int[n];
+        for(int i=0;i<n;i++) num[i] = Integer.parseInt(br.readLine());
+
+
+        int answer = 0;
+        int cnt =1;
+        for(int i=0;i<n;i++){
+            if(i==0 || num[i]<=num[i-1]){
+                cnt=1;
+            }else{
+                cnt++;
+            }
+
+            answer = Math.max(answer, cnt);
+        }
+
+        System.out.println(answer);
+    }
+}
