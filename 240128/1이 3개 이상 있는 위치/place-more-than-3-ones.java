aef5c796ec5920311edfa34676149d11619cded7
vovo-2 <153492321+vovo-2@users.noreply.github.com>
2024-01-28 23:29:14 +0900

    Title: 1이 3개 이상 있는 위치, Time: 95ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240128/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    public static boolean in_range(int x, int y, int N){
+        return 0<= y && y<N && 0<=x && x<N;
+    }
+    public static void main(String[] args) throws Exception {
+        int N = Integer.parseInt(br.readLine());
+        int adj[][] = new int[N][N];
+
+        for(int y=0;y<N;y++){
+            st = new StringTokenizer(br.readLine());
+
+            for(int x=0;x<N;x++){
+                adj[y][x] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int answer = 0;
+        int dy[] = {-1,1,0,0}; //상하좌우 
+        int dx[] = {0,0,-1,1};
+        for(int y=0;y<N;y++){
+                int cnt = 0;
+            for(int x=0;x<N;x++){
+                for(int i=0;i<4;i++){
+                    int nx  = x+dx[i];
+                    int ny = y+dy[i];
+                    if(in_range(nx,ny,N) && adj[ny][nx]==1) cnt++;
+                }
+            }
+            if(cnt>=3) answer++;
+        }
+        System.out.println(answer);
+    }
+}
